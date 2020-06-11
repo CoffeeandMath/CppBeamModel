bcf5e9cd63aa98bf3600b304699c881a6b61e4f0
Kevin <kkorner@caltech.edu>
2020-06-10 23:36:52 -0700

    made changes

--- BeamModel2/main.cpp
+++ BeamModel2/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <quaternion.h>
 #include <Eigen/Dense>
+#include <unsupported/Eigen/CXX11/Tensor>
 using namespace std;
 using namespace Eigen;
 
@@ -20,11 +21,17 @@ int main() {
     m2 = m2*10.0;
     Vector3d m3;
     m3 << 1,1,1;
-    for (int i=0; i<1000; i++){
+    for (int i=0; i<10000; i++){
         m3 = m2.partialPivLu().solve(m3);
     }
 
+    Eigen::Tensor<double,5> m4(3,3,3,3,3);
+    m4.setZero();
+    m4(0,1,2,0,1) = 1.0;
+    m4(2,1,0,2,1) = 2.0;
+    m1.cross(m3);
 
-    cout << m3 << endl;
+    cout << m3.rows() << endl;
+    cout << m4 << endl;
 	return 0;
 }
